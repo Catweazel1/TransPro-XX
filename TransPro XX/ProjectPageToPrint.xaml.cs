645c33b8ee6e76786a1574eb3377d84162d15863
Lonnie Linde <lonnie_linde@hotmail.com>
2019-02-20 15:44:32 +0100

    Design made for printable project page

--- TransPro XX/ProjectPageToPrint.xaml.cs
+++ TransPro XX/ProjectPageToPrint.xaml.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Graphics.Printing;
-using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,8 +12,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using static TransPro_XX.MainPage;
-
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace TransPro_XX
@@ -23,12 +19,11 @@ namespace TransPro_XX
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Settings : Page
+    public sealed partial class ProjectPageToPrint : Page
     {
-        public Settings()
+        public ProjectPageToPrint()
         {
             this.InitializeComponent();
         }
     }
-
 }
